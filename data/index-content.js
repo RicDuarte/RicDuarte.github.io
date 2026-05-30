a793188f3de86f8371530dd7fad2db25de28e3fd
Ricardo Duarte <37026065+RicDuarte@users.noreply.github.com>
2026-05-30 11:44:52 +0100

    Create index-content.js

---
+++ data/index-content.js
@@ -0,0 +1,39 @@
+const content = {
+
+    pt: {
+
+        aboutTitle: "Sobre Mim",
+
+        aboutText1:
+            "Trabalho profissionalmente na área da tradução audiovisual.",
+
+        aboutText2:
+            "Tenho experiência em localização, MTPE e revisão.",
+
+        experienceTitle:
+            "Experiência",
+
+        skillsTitle:
+            "Competências"
+
+    },
+
+    en: {
+
+        aboutTitle: "About Me",
+
+        aboutText1:
+            "I work professionally in audiovisual translation.",
+
+        aboutText2:
+            "I have experience in localization, MTPE and quality control.",
+
+        experienceTitle:
+            "Experience",
+
+        skillsTitle:
+            "Skills"
+
+    }
+
+};
