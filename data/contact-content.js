9db70439d6bc8bb78454552ab3211ea1bfe4f36a
Ricardo Duarte <37026065+RicDuarte@users.noreply.github.com>
2026-06-03 10:02:28 +0100

    Create contact-content.js

---
+++ data/contact-content.js
@@ -0,0 +1,53 @@
+const contactContent = {
+
+    pt: {
+
+        title:
+            "Contactos",
+
+        description:
+            "Se pretende entrar em contacto para projetos de tradução audiovisual, localização, controlo de qualidade ou outras colaborações, utilize um dos canais abaixo ou envie uma mensagem.",
+
+        nameLabel:
+            "Nome",
+
+        emailLabel:
+            "Email",
+
+        subjectLabel:
+            "Assunto",
+
+        messageLabel:
+            "Mensagem",
+
+        submitButton:
+            "Enviar Mensagem"
+
+    },
+
+    en: {
+
+        title:
+            "Contact",
+
+        description:
+            "If you would like to discuss audiovisual translation, localization, quality control or other collaborations, feel free to use one of the channels below or send a message.",
+
+        nameLabel:
+            "Name",
+
+        emailLabel:
+            "Email",
+
+        subjectLabel:
+            "Subject",
+
+        messageLabel:
+            "Message",
+
+        submitButton:
+            "Send Message"
+
+    }
+
+};
