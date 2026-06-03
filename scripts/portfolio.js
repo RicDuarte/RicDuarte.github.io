199934df4054f57dc08ee6f06ce925553b2ac2dd
Ricardo Duarte <37026065+RicDuarte@users.noreply.github.com>
2026-06-03 12:43:43 +0100

    Create portfolio.js

---
+++ scripts/portfolio.js
@@ -0,0 +1,18 @@
+function updatePortfolio(lang) {
+
+    document
+        .getElementById("portfolio-subtitle")
+        .textContent =
+        portfolioContent[lang].heroSubtitle;
+
+    document
+        .getElementById("portfolio-title")
+        .textContent =
+        portfolioContent[lang].heroTitle;
+
+    document
+        .getElementById("portfolio-description")
+        .textContent =
+        portfolioContent[lang].heroDescription;
+
+}
