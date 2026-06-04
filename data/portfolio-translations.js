331e18126534bc3b5c1da733700f501c3cbb7b44
Ricardo Duarte <37026065+RicDuarte@users.noreply.github.com>
2026-06-04 17:13:43 +0100

    Create portfolio-translations.js

---
+++ data/portfolio-translations.js
@@ -0,0 +1,87 @@
+const portfolioTranslations = {
+
+    pt: {
+
+        movie:
+            "Filme",
+
+        series:
+            "Série",
+
+        miniseries:
+            "Minissérie",
+
+        crime:
+            "Crime",
+
+        thriller:
+            "Thriller",
+
+        mystery:
+            "Mistério",
+
+        drama:
+            "Drama",
+      
+        spanish:
+            "Espanhol",
+
+        horror:
+            "Terror",
+
+        adventure:
+          "Aventura",
+
+        documentary:
+          "Documentário",
+
+        animation:
+          "Animação",
+
+        kids:
+          "Crianças"
+    },
+
+    en: {
+
+        movie:
+            "Movie",
+
+        series:
+            "Series",
+
+        miniseries:
+            "Miniseries",
+
+        crime:
+            "Crime",
+
+        thriller:
+            "Thriller",
+
+        mystery:
+            "Mystery",
+
+        drama:
+            "Drama",
+
+        spanish:
+            "Spanish",
+
+        horror:
+            "Horror",
+
+        adventure:
+            "Adventure",
+
+        documentary:
+            "Documentary",
+
+        animation:
+            "Animation",
+
+        kids:
+            "Kids"
+    }
+
+};
