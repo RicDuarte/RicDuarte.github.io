b445f53cf8b333373378c45a98eb82970679c5ee
Ricardo Duarte <37026065+RicDuarte@users.noreply.github.com>
2026-06-04 17:26:24 +0100

    Update portfolio-translations.js

--- data/portfolio-translations.js
+++ data/portfolio-translations.js
@@ -1,85 +1,83 @@
 const portfolioTranslations = {
 
     pt: {
-
         movie:
-            "Filme",
+        "Filme",
 
         series:
             "Série",
-
+    
         miniseries:
             "Minissérie",
-
+    
         crime:
             "Crime",
-
+    
         thriller:
             "Thriller",
-
+    
         mystery:
             "Mistério",
-
+    
         drama:
             "Drama",
-      
+    
         spanish:
             "Espanhol",
-
+    
         horror:
             "Terror",
-
+    
         adventure:
-          "Aventura",
-
+            "Aventura",
+    
         documentary:
-          "Documentário",
-
+            "Documentário",
+    
         animation:
-          "Animação",
-
+            "Animação",
+    
         kids:
-          "Crianças"
+            "Crianças"
     },
 
     en: {
-
         movie:
-            "Movie",
+        "Movie",
 
         series:
             "Series",
-
+    
         miniseries:
             "Miniseries",
-
+    
         crime:
             "Crime",
-
+    
         thriller:
             "Thriller",
-
+    
         mystery:
             "Mystery",
-
+    
         drama:
             "Drama",
-
+    
         spanish:
             "Spanish",
-
+    
         horror:
             "Horror",
-
+    
         adventure:
             "Adventure",
-
+    
         documentary:
             "Documentary",
-
+    
         animation:
             "Animation",
-
+    
         kids:
             "Kids"
     }
